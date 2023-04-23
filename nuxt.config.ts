e75f0034d6fdbc82a27a369ebd2aa684ef5a6ac3
Vento <github@brenneisen.me>
2023-04-23 22:06:10 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,19 @@
 export default defineNuxtConfig({
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/css/main.scss'],
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
+
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-  modules: ['@nuxtjs/strapi', '@pinia/nuxt'],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/strapi',
+    '@pinia/nuxt'
+  ],
+
   strapi: {
     url: process.env.STRAPI_URL || 'http://localhost:1337',
     prefix: '',
@@ -14,6 +22,7 @@ export default defineNuxtConfig({
     cookie: {},
     cookieName: 'strapi_jwt',
   },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
   typescript: {
