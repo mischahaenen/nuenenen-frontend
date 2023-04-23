00091d798323ec1a79ee2e58d7c4bead2d76871a
Vento <github@brenneisen.me>
2023-04-24 00:54:07 +0200

    Update RichTextComponent.vue

--- components/RichTextComponent.vue
+++ components/RichTextComponent.vue
@@ -15,6 +15,7 @@
     </div>
   </div>
 </template>
+
 <script lang="ts" setup>
 import edjsHTML from 'editorjs-html'
 const props = defineProps<{
@@ -25,7 +26,8 @@ const props = defineProps<{
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const htmlContent = edjsHTML().parseStrict(JSON.parse(props.content))
 </script>
-<style lang="scss" scoped>
+
+<style scoped lang="scss">
 .preview :deep(p) {
   overflow: hidden;
   display: -webkit-box;
