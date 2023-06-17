eb1c5936f0410c88682ca363e67e431f90841aec
Vento <github@brenneisen.me>
2023-06-17 22:29:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import express from 'express'
-import { Nuxt } from 'nuxt'
+const express = require('express');
+const { Nuxt, Builder } = require('nuxt');
 
-const config = require('./nuxt.config.js')
+const config = require('./nuxt.config.js');
 
 // Create new express app
-const app = express()
+const app = express();
 
 // Listen to port 3000 or PORT env if provided
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
 
 // Enable production mode
-config.dev = false
+config.dev = false;
 
 // Create instance of nuxt
-const nuxt = new Nuxt(config)
+const nuxt = new Nuxt(config);
 
 // Add nuxt middleware
-app.use(nuxt.render)
+app.use(nuxt.render);
