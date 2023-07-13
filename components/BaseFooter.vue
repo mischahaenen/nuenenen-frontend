3273349e3d6f95213675be618abacc33c189f51f
Vento <github@brenneisen.me>
2023-07-13 22:09:18 +0200

    Add matomo tracking code

--- components/BaseFooter.vue
+++ components/BaseFooter.vue
@@ -42,9 +42,21 @@
   </footer>
 </template>
 
-<script lang="ts" setup>
+<script setup>
 const footerRes = await getFooter()
 const footer = footerRes.data.attributes || null
+
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="https://stat.pfadi-nuenenen.ch/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '1']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
 </script>
 
 <style scoped lang="scss">
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,21 +6,4 @@
     </main>
     <BaseFooter />
   </div>
-  <!-- Matomo -->
-  <script type="application/javascript">
-      var _paq = window._paq = window._paq || [];
-      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-      _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-      _paq.push(["setCookieDomain", "*.www.aula2023.ch"]);
-      _paq.push(['trackPageView']);
-      _paq.push(['enableLinkTracking']);
-      (function() {
-        var u="https://stat.pfadi-nuenenen.ch/";
-        _paq.push(['setTrackerUrl', u+'matomo.php']);
-        _paq.push(['setSiteId', '1']);
-        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-        g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-      })();
-  </script>
-  <!-- End Matomo Code -->
 </template>
